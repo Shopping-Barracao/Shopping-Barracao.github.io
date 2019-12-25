65802d7d2fc13ed1bdf873ad800868261568b8ff
Laiz Lira <laizgamalira@gmail.com>
2019-12-25 13:38:57 -0300

    Outro form

--- send_form_email.php
+++ send_form_email.php
@@ -1,77 +1,78 @@
 <?php
-if(isset($_POST['email'])) {
- 
+/*
+This first bit sets the email address that you want the form to be submitted to.
+You will need to change this value to a valid email address that you can access.
+*/
+$webmaster_email = "laizgamalira@gmail.com";
 
-    $email_to = "laizgamalira@gmail.com";
-    $email_subject = "Contato pelo site";
- 
-  //   // validation expected data exists
-  //   if(!isset($_POST['fname']) ||
-  //       !isset($_POST['lname']) ||
-  //       !isset($_POST['email']) ||
-  //       !isset($_POST['message'])) {
-  //       died('Desculpe, houve um problema com o envio.');       
-  //   }
- 
-     
- 
-  //   $fname = $_POST['fname']; // required
-  //   $lname = $_POST['lname']; // required
-  //   $email_from = $_POST['email']; // required
-  //   $message = $_POST['message']; // required
- 
-  //   $error_message = "";
-  //   $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
- 
-  // if(!preg_match($email_exp,$email_from)) {
-  //   $error_message .= 'Esse e-mail não é válido.<br />';
-  // }
- 
-  //   $string_exp = "/^[A-Za-z .'-]+$/";
- 
-  // if(!preg_match($string_exp,$fname)) {
-  //   $error_message .= 'The First Name you entered does not appear to be valid.<br />';
-  // }
- 
-  // if(!preg_match($string_exp,$lname)) {
-  //   $error_message .= 'The Last Name you entered does not appear to be valid.<br />';
-  // }
- 
-  // if(strlen($message) < 2) {
-  //   $error_message .= 'The Comments you entered do not appear to be valid.<br />';
-  // }
- 
-  // if(strlen($error_message) > 0) {
-  //   died($error_message);
-  // }
- 
-  //   $email_message = "Form details below.\n\n";
- 
-     
-  //   function clean_string($string) {
-  //     $bad = array("content-type","bcc:","to:","cc:","href");
-  //     return str_replace($bad,"",$string);
-  //   }
- 
-     
- 
-  //   $email_message .= "First Name: ".clean_string($fname)."\n";
-  //   $email_message .= "Last Name: ".clean_string($lname)."\n";
-  //   $email_message .= "Email: ".clean_string($email_from)."\n";
-  //   $email_message .= "Mensagem: ".clean_string($message)."\n";
- 
-// create email headers
-$headers = 'From: '.$email_from."\r\n".
-'Reply-To: '.$email_from."\r\n" .
-'X-Mailer: PHP/' . phpversion();
-@mail($email_to, $email_subject, $email_message, $headers);  
-?>
- 
-<!-- include your own success html here -->
- 
-Thank you for contacting us. We will be in touch with you very soon.
- 
-<?php
- 
+/*
+This bit sets the URLs of the supporting pages.
+If you change the names of any of the pages, you will need to change the values here.
+*/
+$feedback_page = "index.html";
+$error_page = "error_message.html";
+$thankyou_page = "thank_you.html";
+
+/*
+This next bit loads the form field data into variables.
+If you add a form field, you will need to add it here.
+*/
+$email = $_REQUEST['email'] ;
+$message = $_REQUEST['message'] ;
+$fname = $_REQUEST['fname'] ;
+$lname = $_REQUEST['lname'] ;
+$msg = 
+"First Name: " . $fname . "\r\n" . 
+"Last name: " . $lname . "\r\n" .
+"Email: " . $email . "\r\n" . 
+"Message: " . $message ;
+
+/*
+The following function checks for email injection.
+Specifically, it checks for carriage returns - typically used by spammers to inject a CC list.
+*/
+function isInjected($str) {
+	$injections = array('(\n+)',
+	'(\r+)',
+	'(\t+)',
+	'(%0A+)',
+	'(%0D+)',
+	'(%08+)',
+	'(%09+)'
+	);
+	$inject = join('|', $injections);
+	$inject = "/$inject/i";
+	if(preg_match($inject,$str)) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+// If the user tries to access this script directly, redirect them to the feedback form,
+if (!isset($_REQUEST['email'])) {
+header( "Location: $feedback_page" );
+}
+
+// If the form fields are empty, redirect to the error page.
+elseif (empty($fname) || empty($email)) {
+header( "Location: $error_page" );
+}
+
+/* 
+If email injection is detected, redirect to the error page.
+If you add a form field, you should add it here.
+*/
+elseif ( isInjected($email) || isInjected($fname)  || isInjected($message) ) {
+header( "Location: $error_page" );
+}
+
+// If we passed all previous tests, send the email then redirect to the thank you page.
+else {
+
+	mail( "$webmaster_email", "Feedback Form Results", $msg );
+
+	header( "Location: $thankyou_page" );
 }
 ?>
